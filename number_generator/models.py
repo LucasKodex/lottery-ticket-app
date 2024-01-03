d6873e35c20a285bf7fe7d195f21f6df9b7e4ea5
Lucas G. Cordeiro <lucas.kodex.dev@gmail.com>
2024-01-02 21:43:53 -0400

    feat: starts home page and number generation

--- number_generator/models.py
+++ number_generator/models.py
@@ -1,11 +1,34 @@
 from django.db import models
+from uuid import uuid4
+from random import seed, randint, choice
 
 class Generation(models.Model):
     guid = models.UUIDField(primary_key=True)
     public_unique_identifier = models.PositiveIntegerField(unique=True)
     range_from = models.PositiveSmallIntegerField()
     range_to = models.PositiveSmallIntegerField()
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def generateRandomNumbers(
+        self,
+        quantity: int,
+    ):
+        seed()
+        numbers = list()
+        generatedNumbers = set()
+
+        while len(generatedNumbers) < quantity:
+            randomInteger = randint(self.range_from, self.range_to)
+            generatedNumbers.add(randomInteger)
+        
+        for generatedNumber in generatedNumbers:
+            number = Number()
+            number.generation = self.guid
+            number.number = generatedNumber
+            number.color = Number.randomColorEnum()
+            numbers.insert(number)
+        
+        return numbers
 
 class Number(models.Model):
     COLOR_ENUM = {
@@ -20,3 +43,6 @@ class Number(models.Model):
     color = models.CharField(max_length=6, choices=COLOR_ENUM)
     number = models.PositiveSmallIntegerField()
 
+    @staticmethod
+    def randomColorEnum():
+        return choice(list(Number.COLOR_ENUM.keys()))
--- number_generator/views.py
+++ number_generator/views.py
@@ -1,6 +1,9 @@
 from typing import Any
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView, FormView, ListView, RedirectView, TemplateView
+from django.db import IntegrityError, transaction
+
+from .models import Generation
 
 APP_NAME = "number_generator"
 
@@ -8,9 +11,65 @@ class RedirectToHomeView(RedirectView):
     pattern_name = f"{APP_NAME}:home_page"
     permanent = True
 
-# class HomeView(FormView):
 class HomeView(TemplateView):
     template_name = f"{APP_NAME}/home.html"
+
+def homeViewPOST(request):
+    TEMPLATE_NAME = f"{APP_NAME}/home.html"
+
+    quantity = request.POST["quantity"]
+    range_from = request.POST["range_from"]
+    range_to = request.POST["range_to"]
+
+    isInputDecimal = quantity.isdecimal() and range_from.isdecimal() and range_to.isdecimal()
+    if not isInputDecimal:
+        return render(
+            request,
+            TEMPLATE_NAME,
+            {
+                "error_list": [ "Fields must be integer numbers." ],
+            },
+        )
+
+    quantity = int(request.POST["quantity"])
+    range_from = int(request.POST["range_from"])
+    range_to = int(request.POST["range_to"])
+
+    try:
+        with transaction.atomic():
+            generation = Generation()
+            generation.range_from = range_from
+            generation.range_to = range_to
+            generation.save()
+
+            randomNumbers = generation.generateRandomNumbers(quantity)
+            for number in randomNumbers:
+                number.save()
+    except IntegrityError:
+        return render(
+            request,
+            TEMPLATE_NAME,
+            context={
+                "error_list": [
+                    "Something went wrong :(. Contact an \
+                    administrator and trying again later."
+                ],
+            },
+            status=500,
+        )
+    return redirect(
+        "number_generator:generation_detail_page",
+        # args=(generation.public_unique_identifier),
+    )
+
+def homeViewALL(request):
+    TEMPLATE_NAME = f"{APP_NAME}/home.html"
+    return render(request, TEMPLATE_NAME)
+
+def homeView(request):
+    if request.method == "POST":
+        return homeViewPOST(request)
+    return homeViewALL(request)
 
 # class GenerationDetailView(DetailView):
 class GenerationDetailView(TemplateView):
